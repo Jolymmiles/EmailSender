eefe0b54b2c1f0c2f37268e1ba3bd7c1a8a4e3a0
Valentine <valmak29@gmail.com>
2022-09-22 17:13:40 +0400

    Change singleton on scoped

--- EmailSender/Program.cs
+++ EmailSender/Program.cs
@@ -13,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddSingleton<IMessageService, MessageService>();
+builder.Services.AddScoped<IMessageService, MessageService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services
